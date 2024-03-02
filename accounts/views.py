165385896fe68fa96851a1f0312a9e3b54f898c4
BDlhj <akk808@snu.ac.kr>
2024-03-02 20:23:08 +0900

    feat: Implement user signout

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,10 @@
 from django.utils import timezone
-from rest_framework import generics
+from rest_framework import generics, status
 from rest_framework.authtoken.models import Token
 from rest_framework.authtoken.views import ObtainAuthToken
 from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
 from accounts.models import User
 from accounts.serializers import SignupSerializer
 
@@ -26,3 +28,11 @@ class SigninView(ObtainAuthToken):
             'user_id': user.pk,
             'token': token.key
         })
+
+
+class SignoutView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        request.user.auth_token.delete()
+        return Response(status=status.HTTP_200_OK)
