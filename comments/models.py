b4057fc3753dec5b98953430cea8a14a5829b499
BDlhj <akk808@snu.ac.kr>
2024-03-04 08:01:52 +0900

    feat: Implement creating tags when creating a comment

--- comments/models.py
+++ comments/models.py
@@ -7,6 +7,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
+    tags = models.ManyToManyField("tags.Tag", through="tags.TagCommentSet", related_name="comments")
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_updated = models.BooleanField(default=False)
--- comments/serializers.py
+++ comments/serializers.py
@@ -1,20 +1,31 @@
 from rest_framework import serializers
 from comments.models import Comment
+from tags.models import Tag
+from tags.serializers import TagSerializer
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.PrimaryKeyRelatedField(read_only=True)
     post = serializers.PrimaryKeyRelatedField(read_only=True)
+    tags = serializers.ListField(child=serializers.CharField(), write_only=True)
 
     class Meta:
         model = Comment
         fields = "__all__"
 
+    def create(self, validated_data):
+        tags_list = validated_data.pop("tags", [])
+        comment = Comment.objects.create(**validated_data)
 
-class CommentDetailSerializer(serializers.ModelSerializer):
-    author = serializers.PrimaryKeyRelatedField(read_only=True)
-    post = serializers.PrimaryKeyRelatedField(read_only=True)
+        for tag in tags_list:
+            tag, created = Tag.objects.get_or_create(content=tag)
 
-    class Meta:
-        model = Comment
-        fields = "__all__"
+        comment.tags.set(tags_list)
+        
+        return comment
+    
+    def to_representation(self, instance):
+        res = super().to_representation(instance)
+        tags = instance.tags.all()
+        res.update({'tags': [TagSerializer(tag).data["content"] for tag in tags]})
+        return res
