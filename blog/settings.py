9c32816c700a3e5b496304d17b4f4054c801a9cb
BDlhj <akk808@snu.ac.kr>
2024-03-05 01:02:10 +0900

    conf: Change database

--- blog/settings.py
+++ blog/settings.py
@@ -86,9 +86,13 @@ WSGI_APPLICATION = "blog.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env('NAME'),
+        'USER': env('USER'),
+        'PASSWORD': env('PASSWORD'),
+        'HOST': env('HOST'),
+        'PORT': env('PORT'),
     }
 }
 
